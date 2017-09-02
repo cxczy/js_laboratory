c7580b66fca786b9913fbb173b08e28f6f2cf52e
cxczy <1050079972@qq.com>
2017-09-03 00:03:43 +0800

    ES6 environment

---
+++ App.js
@@ -0,0 +1,12 @@
+class playground {
+    constructor() {
+        this.version = '1.0'
+        this.author = 'cxczy'
+    }
+
+    getInfo() {
+        console.log(`version: ${this.version}`)
+    }
+}
+
+export default playground
---
+++ main.js
@@ -0,0 +1,4 @@
+import App from './App';
+
+var test = new App()
+test.getInfo()
